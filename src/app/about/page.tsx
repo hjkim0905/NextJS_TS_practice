488412577cde4097df2ccf37e8527e1f7d9280ef
hjkim0905 <eax9952@gmail.com>
2025-03-11 13:19:25 +0900

    [Feat] : Css studying

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,11 @@
 export default function About() {
     return (
-        <div>
-            <h1>About Us</h1>
-            <p>Welcome to our website. We are dedicated to providing the best service possible.</p>
+        <div className="flex min-h-screen flex-col items-center p-24">
+            <h1 className="text-4xl font-bold mb-8">About Us</h1>
+            <p className="text-lg text-center max-w-2xl">
+                Welcome to our website. We are dedicated to providing the best service possible. Our team works
+                tirelessly to ensure that every user has an amazing experience.
+            </p>
         </div>
     );
 }
