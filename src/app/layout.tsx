2c289420dd6814b436fe55afd02cc42935eaa809
hjkim0905 <eax9952@gmail.com>
2025-03-12 14:25:49 +0900

    [Feat] : Studying css with official next.js document

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
 import '@/app/globals.css';
 import React from 'react';
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                {/* Layout UI */}
+                {/* Place children where you want to render a page or nested layout */}
+                <main>{children}</main>
+            </body>
         </html>
     );
 }
